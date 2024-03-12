c9eb91f1821e9b5aee66d9a5777b370e3c6c4ba1
Homework-10 <danilovsansan@gmail.com>
2024-03-12 19:35:43 +0300

    Сравнение паролей

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        checkLoginPassword("login", "asdafsfbsjskfb", "password");
+        try {
+            checkLoginPassword("lotyhsrtyxfhs", "12456789", "12222222222222223456789");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void checkLoginPassword(String login,String password,String confirmPassword) {
@@ -10,6 +14,16 @@ public class Main {
         }
         if (!login.matches("[a-zA-Z0-9_]+")) {
             throw new WrongLoginException("Недопустимый символ");
+        }
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Слишком длинный пароль");
+        }
+        if (!password.matches("[a-zA-Z0-9_]+")) {
+            throw new WrongPasswordException("Недопустимый  символ");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException(" пароли неодинаковы");
+
         }
 
 
